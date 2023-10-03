f67a04b52b96a1a5a773c8e13addb374207d9938
QIAIUNCC <146682162+QIAIUNCC@users.noreply.github.com>
2023-10-03 09:01:06 -0400

    Update split_data.py

--- utils/split_data.py
+++ utils/split_data.py
@@ -5,21 +5,19 @@ import random
 from PIL import Image
 from natsort import natsorted
 
-new_root = "/home/sgholami/Downloads/test"
+new_root = "path to new root"
 
-
+### Move x% of data from root to new_root
 def move_data(img_path):
     image = Image.open(img_path)
     img_path = img_path.replace("\\", "/")
-    # p2 = os.path.join(root, img.split("-")[0], img.split("-")[0]+img.split(img.split("-")[2]))
     p2 = os.path.join(new_root, img_path.split("/")[-2], img_path.split("/")[-1])
     image.save(p2)
     os.remove(img_path)
 
 
 if __name__ == "__main__":
-    root = "/home/sgholami/Downloads/kermany_full_no_duplicates"
-    # root = "/sina/flower/data/3dataset/0"
+    root = "path to current root"
     image_format = ".jpeg"
     nproc = 8
     percentage = 0.05
@@ -55,22 +53,9 @@ if __name__ == "__main__":
         val_data_len = math.floor(len(imgs_dict.keys()) * percentage)
         keys = list(imgs_dict.keys())
         for key, val in imgs_dict.items():
-            # if key not in keys[:val_data_len]:
-            #     train_img_ids += [img_file_path + "/" + key + count for count in val]
-            #     print(train_img_ids)
             if key in keys[:val_data_len]:
                 test_img_ids += [img_file_path + "/" + key + count for count in val]
-    # images = os.listdir(os.path.join(root, files[0]))
-    # images_idx = random.sample(range(0, len(images)), math.floor(len(images) * percentage))
-    # tasks = []
-    # for i in images_idx:
-    #     img_name = images[i].split(image_format)[0]
-    #     tasks.append({"root": root,
-    #                   "files": files,
-    #                   "img_name": img_name,
-    #                   "image_format": image_format,
-    #                   "out_suffix": out_suffix})
-    #
+  
     if nproc > 1:  # handle it in parallel or not
         mmcv.track_parallel_progress(move_data, test_img_ids, nproc)
     else:
